789e86bfc53934ed043b285b631f8c2ffc3a5486
MohammadHossein Shahmohammady <hos1377@gmail.com>
2019-04-11 23:33:20 +0430

    Add: XSS attack

--- post/models.py
+++ post/models.py
@@ -4,7 +4,9 @@ from sqlite3 import Error, IntegrityError, OperationalError
 from manager import db_path
 
 from .utils import convert_to_tehran
-
+from db.injection import escape_sql_injection
+from db import xss
+xss_cleaner = xss.XssCleaner()
 
 def create_new_post(content, username):
     conn = sqlite3.connect(db_path)
@@ -12,6 +14,7 @@ def create_new_post(content, username):
     content = escape_sql_injection(content)
     username = escape_sql_injection(username)
     command = f"INSERT INTO posts (content, user_id) VALUES ('{content}', (SELECT id FROM users WHERE username = '{username}'));"
+    print(command)
     crsr.execute(command)
     conn.commit()
     conn.close()
@@ -23,7 +26,7 @@ def get_all_posts():
     command = "select content, users.username, created_date from posts inner join users on users.id = posts.user_id order by -posts.id;"
     posts = crsr.execute(command)
     posts = posts.fetchall()
-    posts = [{'content':content, 'username': username, 'created_date': convert_to_tehran(created_date)} for content, username, created_date in posts]
+    posts = [{'content': xss_cleaner.strip(content), 'username': username, 'created_date': convert_to_tehran(created_date)} for content, username, created_date in posts]
     conn.close()
     return posts
 
