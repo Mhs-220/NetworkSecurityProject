ab68bfa161debae086c23783b83cf59c10e3bfb5
MohammadHossein Shahmohammady <hos1377@gmail.com>
2019-04-19 23:45:25 +0430

    Update: check for xss on input

--- post/models.py
+++ post/models.py
@@ -12,9 +12,9 @@ def create_new_post(content, username):
     conn = sqlite3.connect(db_path)
     crsr = conn.cursor()
     content = escape_sql_injection(content)
+    content = xss_cleaner.strip(content)
     username = escape_sql_injection(username)
     command = f"INSERT INTO posts (content, user_id) VALUES ('{content}', (SELECT id FROM users WHERE username = '{username}'));"
-    print(command)
     crsr.execute(command)
     conn.commit()
     conn.close()
@@ -26,7 +26,7 @@ def get_all_posts():
     command = "select content, users.username, created_date from posts inner join users on users.id = posts.user_id order by -posts.id;"
     posts = crsr.execute(command)
     posts = posts.fetchall()
-    posts = [{'content': xss_cleaner.strip(content), 'username': username, 'created_date': convert_to_tehran(created_date)} for content, username, created_date in posts]
+    posts = [{'content': content, 'username': username, 'created_date': convert_to_tehran(created_date)} for content, username, created_date in posts]
     conn.close()
     return posts
 
